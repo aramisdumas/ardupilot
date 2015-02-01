f43b589caefc6cbb47c0cefa989cef410ea33d3e
aramisdumas <chineseman2699@gmail.com>
2015-02-01 01:04:32 -0500

    Change Firmware Name

--- ArduCopter/ArduCopter.ino
+++ ArduCopter/ArduCopter.ino
@@ -1,7 +1,7 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#define THISFIRMWARE "ArduCopter V3.2.1-rc2"
-/*
+#define THISFIRMWARE "ArduCopter ASD T1 Arthur"
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
